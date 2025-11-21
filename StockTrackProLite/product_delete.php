7f4afcc70662965686a78dcb512013fbe444307e
ta765 <ta765@student.aru.ac.uk>
2025-11-21 12:34:34 +0000

    altered the page to hide deleted products

--- StockTrackProLite/product_delete.php
+++ StockTrackProLite/product_delete.php
@@ -2,6 +2,16 @@
 include 'includes/db.php';
 
 $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
-mysql_query("DELETE FROM products WHERE id=$id");
-header('Location: products.php?msg=deleted');
-exit();
+//mysql_query("DELETE FROM products WHERE id=$id");
+if ($id) {
+    // It sets the active status to 0 for the specified product ID.
+    $sql_hide = "UPDATE products SET is_active = 0 WHERE id = $id";
+    
+    $stmt = mysqli_prepare($conn, $sql_hide);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    header("Location: products_list.php?status=product_hidden");
+    exit();
+}
