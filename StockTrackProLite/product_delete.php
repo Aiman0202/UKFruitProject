0334f8d1d38a3378e20297a2f5296ffe058b4ba0
adam wingell <adam.s.wingell@gmail.com>
2025-12-04 21:21:02 +0000

    updated API for report on stocktrackpro

--- StockTrackProLite/product_delete.php
+++ StockTrackProLite/product_delete.php
@@ -4,7 +4,6 @@ include __DIR__ . '/includes/db.php';
 $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 
 if ($id) {
-    // Set the active status to 0 for the specified product ID.
     $sql_hide = "UPDATE products SET is_active = 0 WHERE id = ?";
     
     $stmt = mysqli_prepare($conn, $sql_hide);
